878c910946dfdeeea13b14a4019c7e6590ca4425
Chris Gill <chris@chrisgill.net>
2025-05-02 16:12:42 -0400

    Add "Suggested Area" as a field for device info.
    Signed-off-by: Chris Gill <chris@chrisgill.net>

--- ha_mqtt_discoverable/__init__.py
+++ ha_mqtt_discoverable/__init__.py
@@ -485,6 +485,8 @@ class DeviceInfo(BaseModel):
     configuration_url: str | None = None
     """A link to the webpage that can manage the configuration of this device.
         Can be either an HTTP or HTTPS link."""
+    suggested_area: str | None = None
+    """ The area to put the device in on creation, if it isn't already in one. """
     via_device: str | None = None
     """Identifier of a device that routes messages between this device and Home
         Assistant. Examples of such devices are hubs, or parent devices of a sub-device.
