1cc357cd56c7491dc261fd045003bf013d2097e5
Steffen Pankratz <kratz00@gmx.de>
2025-06-26 11:49:14 +0200

    [pre-commit.ci] auto fixes from pre-commit.com hooks
    for more information, see https://pre-commit.ci
    
    Signed-off-by: Chris Gill <chris@chrisgill.net>
    Signed-off-by: Steffen Pankratz <kratz00@gmx.de>

--- tests/test_image.py
+++ tests/test_image.py
@@ -30,6 +30,7 @@ def image(request) -> Image:
     settings = Settings(mqtt=mqtt_settings, entity=image_info)
     return Image(settings)
 
+
 def test_required_config():
     """Test to make sure an image instance can be created"""
     mqtt_settings = Settings.MQTT(host="localhost")
@@ -38,21 +39,25 @@ def test_required_config():
     image = Image(settings)
     assert image is not None
 
+
 def test_url_and_image_raises_exception():
     mqtt_settings = Settings.MQTT(host="localhost")
     with pytest.raises(ValueError) as excinfo:
         image_info = ImageInfo(name="test", url_topic="url_to_publish_to", image_topic="image_to_publish_to")
+
 
 def test_url_and_encoding_raises_exception():
     mqtt_settings = Settings.MQTT(host="localhost")
     with pytest.raises(ValueError) as excinfo:
         image_info = ImageInfo(name="test", url_topic="url_to_publish_to", image_encoding="b64")
 
+
 def test_invalid_encoding_raises_exception():
     mqtt_settings = Settings.MQTT(host="localhost")
     # settings = Settings(mqtt=mqtt_settings, entity=image_info)
     with pytest.raises(ValueError) as excinfo:
         image_info = ImageInfo(name="test", image_encoding="invalid_encoding", image_topic="image_to_publish_to")
+
 
 @pytest.mark.parametrize("image", [("topic_to_publish_to", None, None)], indirect=True)
 def test_generate_config_url(image: Image):
@@ -62,6 +67,7 @@ def test_generate_config_url(image: Image):
     if image._entity.url_topic:
         assert config["url_topic"] == image._entity.url_topic
 
+
 @pytest.mark.parametrize("image", [(None, "image_to_publish_to", "b64")], indirect=True)
 def test_generate_config_image(image: Image):
     config = image.generate_config()
@@ -70,6 +76,7 @@ def test_generate_config_image(image: Image):
     if image._entity.image_topic:
         assert config["image_topic"] == image._entity.image_topic
 
+
 @pytest.mark.parametrize("image", [("topic_to_publish_url_to", None, None)], indirect=True)
 def test_set_url(image: Image):
     image_url = "http://camera.local/latest.jpg"
@@ -77,6 +84,7 @@ def test_set_url(image: Image):
     with patch.object(image.mqtt_client, "publish") as mock_publish:
         image.set_url(image_url)
         mock_publish.assert_called_with(image._entity.url_topic, image_url, retain=True)
+
 
 @pytest.mark.parametrize("image", [(None, "image_to_publish_to", "b64")], indirect=True)
 def test_set_blob(image: Image):
@@ -88,10 +96,9 @@ def test_set_blob(image: Image):
         "/+Q7U+SSyJXGYwcC6hCgWj7wf/gd7dGYXLCSQrFgMCLZX2MAMFdoFm3rO9jy2qeAL5n4Ept+6sNYPaT9HpbixwBfdvAxXVbk/aAy"
         "x1g8EkTddGWfDS9hQLwfkbflAMGboHuNae31j5OH4AMdbV8AxwcAqNFyl53eXdXZ2//nmn19wN6HHKqAAggmgAAAAlwSFlzAAAuI"
         "wAALiMBeKU/dgAAAAd0SU1FB+kFBAs3LBSX2/sAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAADElEQVQI1"
-        "2NgYGAAAAAEAAEnNCcKAAAAAElFTkSuQmCC")
+        "2NgYGAAAAAEAAEnNCcKAAAAAElFTkSuQmCC"
+    )
 
     with patch.object(image.mqtt_client, "publish") as mock_publish:
         image.set_image(image_blob)
         mock_publish.assert_called_with(image._entity.image_topic, image_blob, retain=True)
-
-
