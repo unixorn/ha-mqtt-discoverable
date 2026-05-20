963585b49c491f99ef4b39111091822e9d473116
Jeroen van Oosterhout <18647330+jvanoosterhout@users.noreply.github.com>
2026-05-20 15:29:31 +0200

    Update ha_mqtt_discoverable/sensors.py
    Co-authored-by: Steffen Pankratz <kratz00@gmx.de>
    Signed-off-by: Jeroen van Oosterhout <18647330+jvanoosterhout@users.noreply.github.com>

--- ha_mqtt_discoverable/sensors.py
+++ ha_mqtt_discoverable/sensors.py
@@ -233,7 +233,7 @@ class ValveInfo(EntityInfo):
 
         # Don't unset reports_position and payload_close, payload_open, state_open, state_closed at the same time.
         if not self.reports_position and (
-            self.payload_open is None or self.payload_close is None or self.state_open is None or self.state_closed is None
+            self.state_open is None or self.state_closed is None
         ):
             raise ValueError(
                 "payload_open, payload_close, state_open and state_closed should be set when not using reports_position."
